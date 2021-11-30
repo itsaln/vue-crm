6b9d4246b5d87a9bbd8f123a7fa95b211843af6e
scripac <mukhammadiev013@gmail.com>
2021-11-30 12:00:50 +0500

    ...

--- src/store/index.js
+++ src/store/index.js
@@ -24,7 +24,6 @@ export default new Vuex.Store({
       const key = process.env.VUE_APP_FIXER
       const res = await fetch(`http://data.fixer.io/api/latest?access_key=${key}&symbols=USD,EUR,RUB`)
       return await res.json()
-      // const res = await fetch('https://vue-crm-81e42-default-rtdb.asia-southeast1.firebasedatabase.app/')
     }
   },
   getters: {
